dd7442afce8bc9230790083effc1f3790ae66dc9
Alex <alexgrubor@gmail.com>
2023-04-21 09:54:26 +0200

    my friends responsive fix

--- src/pages/Friends.tsx
+++ src/pages/Friends.tsx
@@ -39,38 +39,41 @@ const Friends = () => {
   };
 
   return (
-    <div className="bg-gradient-to-br from-orange-400 to-pink-400 h-screen flex flex-col justify-center items-center px-2">
-  <div className="bg-white rounded-lg shadow-lg p-8 w-full sm:w-2/3 md:w-1/2 lg:w-1/3">
-    <h1 className="text-3xl font-bold mb-4">My Friends</h1>
-    <div className="flex flex-col gap-4">
-      {friends &&
-        friends.map((friend: any) => (
-          <div
-            key={friend._id}
-            className="bg-gray-100 rounded-lg p-4 flex flex-col gap-2"
-          >
-            <p className="text-lg font-medium">
-              {friend.user.firstName} {friend.user.lastName}
-            </p>
-            <p className="text-gray-500">{friend.user.email}</p>
-            <img
-              src={friend.user.profilePicture}
-              alt={friend.user.firstName}
-              className="w-40 h-40 object-cover rounded-full mx-auto"
-            />
-            <button
-              className="bg-red-500 text-white py-2 px-4 rounded-lg hover:bg-red-600 transition-colors duration-200"
-              onClick={() => {
-                deleteFriendHandler(friend._id);
-              }}
+    <div className="bg-gradient-to-br from-orange-400 to-pink-400 h-screen flex flex-col justify-center items-center px-2 pt-20">
+    <div className="bg-white rounded-lg shadow-lg p-8 w-full sm:w-2/3 md:w-1/2 lg:w-1/3">
+      <h1 className="text-3xl font-bold mb-4">My Friends</h1>
+      <div className="flex flex-col gap-4">
+        {friends &&
+          friends.map((friend: any) => (
+            <div
+              key={friend._id}
+              className="bg-gray-100 rounded-lg p-4 flex flex-col gap-2 sm:flex-row sm:items-center"
             >
-              delete friend
-            </button>
-          </div>
-        ))}
+              <img
+                src={friend.user.profilePicture}
+                alt={friend.user.firstName}
+                className="w-40 h-40 object-cover rounded-full mx-auto sm:mx-0 sm:mr-4"
+              />
+              <div className="flex flex-col justify-center">
+                <p className="text-lg font-medium mb-1">
+                  {friend.user.firstName} {friend.user.lastName}
+                </p>
+                <p className="text-gray-500 mb-2">{friend.user.email}</p>
+                <button
+                  className="bg-red-500 text-white py-2 px-4 rounded-lg hover:bg-red-600 transition-colors duration-200"
+                  onClick={() => {
+                    deleteFriendHandler(friend._id);
+                  }}
+                >
+                  delete friend
+                </button>
+              </div>
+            </div>
+          ))}
+      </div>
     </div>
   </div>
-</div>
+  
   );
 };
 export default Friends;
